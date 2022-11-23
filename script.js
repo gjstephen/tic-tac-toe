cefd7649d2ca8f3ff47ad63ac48eca83a8960a80
gjstephen <gjstephen1@gmail.com>
2022-11-23 14:50:24 +1100

    added tie condition, fixed: diagonal condition / score counter / new game button

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ var pOneScoreDisplay = document.querySelector('.p1-score')
 var pTwoScoreDisplay = document.querySelector('.p2-score')
 var gameGrid = document.querySelector('.game-grid')
 var gridBoxes = document.querySelectorAll('.grid-box')
+var turnDisplay = document.querySelector('.turn-display')
 var resultMsg = document.querySelector('.game-result')
 var resetBtn = document.querySelector('.reset-play')
 var roundDisplay = document.querySelector('.round-tally')
@@ -23,27 +24,38 @@ var newGameBtn = document.querySelector('.new-game')
 
 gameGrid.addEventListener('click', function (event) {
     var targetBox = event.target
-
-    if (winCondition !== true) {
-        // condition that box was clicked
-        if (targetBox.classList.contains('grid-box') === true) {
-            // check if grid is taken
-            if (freeSpace(targetBox) === false) {
-                // check who's turn
-                if (turnCounter % 2 === 0) {
-                    targetBox.classList.toggle('cross-move')
-                    var moveSpace = targetBox.classList[1]
-                    checkWin()
-                    turnCounter += 1
-                    playerMove = 'naught-move'
-
-                } else {
-                    targetBox.classList.toggle('naught-move')
-                    var moveSpace = targetBox.classList[1]
-                    checkWin()
-                    turnCounter += 1
-                    playerMove = 'cross-move'
-
+    if (turnCounter < 10) {
+        if (winCondition !== true) {
+            // condition that box was clicked
+            if (targetBox.classList.contains('grid-box') === true) {
+                // check if grid is taken
+                if (freeSpace(targetBox) === false) {
+                    // check who's turn
+                    if (turnCounter % 2 === 0) {
+                        targetBox.classList.toggle('cross-move')
+                        var moveSpace = targetBox.classList[1]
+                        turnDisplay.textContent = `${pOneName}'s Turn`
+                        checkWin()
+                        turnCounter += 1
+                        if (turnCounter === 10) {
+                            turnDisplay.textContent = `Game Over`
+                            resultMsg.textContent = `Its a tie!`
+                        } else {
+                        playerMove = 'naught-move'
+                        }
+                    } else {
+                        targetBox.classList.toggle('naught-move')
+                        var moveSpace = targetBox.classList[1]
+                        turnDisplay.textContent = `${pTwoName}'s Turn`
+                        checkWin()
+                        turnCounter += 1
+                        if (turnCounter === 10) {
+                            turnDisplay.textContent = `Game Over`
+                            resultMsg.textContent = `Its a tie!`
+                        } else {
+                            playerMove = 'cross-move'
+                        }
+                    }
                 }
             }
         }
@@ -107,7 +119,7 @@ function diagonalWin() {
         }
     }
     var winCounter = 0
-    for (var i = 2; i < 9; i = i + 2) {
+    for (var i = 2; i < 7; i = i + 2) {
         if (gridBoxes[i].classList.contains(playerMove)) {
             winCounter = winCounter + 1
         }
@@ -118,23 +130,30 @@ function diagonalWin() {
     
 }
 
+// check tie condition
+// function checkTie() {
+
+// }
+
 function checkWin() {
     horizontalWin()
     verticalWin()
     diagonalWin()
+    // checkTie()
     if (winCondition === true) {
         if (turnCounter % 2 === 0) {
             resultMsg.textContent = `${pTwoName} wins!`
             pTwoScore = pTwoScore + 1
-            console.log(pTwoScore)
+            // console.log(pTwoScore)
             pTwoScoreDisplay.textContent = `Score: ${pTwoScore}` 
+            turnDisplay.textContent = `Game Over`
         } else {
             resultMsg.textContent = `${pOneName} wins!`
             pOneScore = pOneScore + 1
-            console.log(pOneScore)
+            // console.log(pOneScore)
             pOneScoreDisplay.textContent = `Score: ${pOneScore}`
+            turnDisplay.textContent = `Game Over`
         }
-        
     }
 }
 
@@ -171,12 +190,11 @@ newGameBtn.addEventListener('click', function (event) {
     pOneScore = 0
     pOneScoreDisplay.textContent = `Score: ${pOneScore}`
     pTwoScore = 0
-    pTwoScoreDisplay = `Score: ${pTwoScore}`
+    pTwoScoreDisplay.textContent = `Score: ${pTwoScore}`
     pOneName = 'Player One'
     oneNameText.textContent = pOneName
     pTwoName = 'Player Two'
     twoNameText.textContent = pTwoName
-    // console.log
 })
 
 // change player1 name
@@ -185,8 +203,10 @@ var oneNameBtn = document.querySelector('.p1-name-btn')
 var oneNameText = document.querySelector('.p1-name-text')
 oneNameBtn.addEventListener('click', function (event) {
     if (oneNameInput.value.length > 0) {
-        oneNameText.textContent = oneNameInput.value
+        pOneName = oneNameInput.value
+        oneNameText.textContent = pOneName
         oneNameInput.value = ''
+        turnDisplay.textContent = `${pOneName}'s Turn`
     }
 })
 
@@ -195,7 +215,8 @@ var twoNameBtn = document.querySelector('.p2-name-btn')
 var twoNameText = document.querySelector('.p2-name-text')
 twoNameBtn.addEventListener('click', function (event) {
     if (twoNameInput.value.length > 0) {
-        twoNameText.textContent = twoNameInput.value
+        pTwoName = twoNameInput.value
+        twoNameText.textContent = pTwoName
         twoNameInput.value = ''
     }
 })
